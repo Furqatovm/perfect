fbed90b700cc67a956fa03122243af0562702f06
Furqatovm <muhammadyusuffurqatov91@gmail.com>
2024-12-28 13:21:11 +0500

    add new photo changer

--- main.js
+++ main.js
@@ -11,23 +11,44 @@ let img4 =document.getElementById("men_ozim");
 img4.style.display ="none";
 
 let tugma =document.getElementById("right-aray");
+let tugma2 =document.getElementById("right-images")
 
-tugma.addEventListener("click", function (){
-    img1.style.display ="none";
-    img3.style.display ="block"
+tugma.addEventListener("click", function(){
+    if (img1.style.display == "block") {
+        img2.style.display = "block";
+        img1.style.display ="none";
+    } else if (img2.style.display =="block") {
+        img3.style.display ="block";
+        img2.style.display ="none";
+    } else if (img3.style.display =="block") {
+        img3.style.display ="none";
+        img4.style.display ="block";
+    } else {
+        img1.style.display =="block"
+    }
 });
 
-// tugma.addEventListener("click", function (){
-//     img2.style.display ="none";
-//     img3.style.display ="block"
-// });
-let tugma2 =document.getElementById("hero-right-button");
 
-tugma2.addEventListener("click", function (){
-    img1.style.display ="block";
-    img3.style.display ="none"
-});
+
+tugma2.addEventListener("click", function(){
+    if (img2.style.display == "block") {
+        img1.style.display = "block";
+        img2.style.display ="none";
+    } else if (img3.style.display =="block") {
+        img2.style.display ="block";
+        img3.style.display ="none";
+    } else if (img4.style.display =="block") {
+        img4.style.display ="none";
+        img3.style.display ="block";
+    } else {
+        img1.style.display =="block"
+    }
+})
+
+
 
 let elements = document.getElementById("nav-child");
 let nav =document.querySelector("nav");
 elements.style.display ="none";
+
+
